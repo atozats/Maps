0c14b9424d1dd07acc7c64af2064651b6f2987a4
bhavishya <bhavishya.kerpada@gmail.com>
2025-03-21 14:35:52 +0530

    sendsms.js file updated

--- server/sendsms.js
+++ server/sendsms.js
@@ -1,19 +1,21 @@
 import axios from "axios";
+import dotenv from "dotenv";
 
- const sendsms = async (PHONE, OTP) => {
+// Load environment variables from the .env file
+dotenv.config();
 
-
-  const API = "936824c0191953647ec609b4f49bc964";
+const sendsms = async (PHONE, OTP) => {
+  // Get the API key from the environment variable
+  const API = process.env.API_KEY;
   const URL = `https://sms.renflair.in/V1.php?API=${API}&PHONE=${PHONE}&OTP=${OTP}`;
 
-  await axios.get(URL)
-    .then(response => {
-      const data = response.data;
-      console.log(data);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+  try {
+    const response = await axios.get(URL);
+    const data = response.data;
+    console.log(data);
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
 
 export default sendsms;
